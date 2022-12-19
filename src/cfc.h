1d8ce7a483159b91dd28de22d55086dcf96da1f5
Liviu Nicoara <lnicoara at thinkoid dot org>
2022-12-19 17:17:43 -0500

    chore: remove unused macros

--- src/cfc.h
+++ src/cfc.h
@@ -5,11 +5,6 @@
 
 #include <ccittfax/defs.h>
 
-#define UNUSED(x) ((void)(x))
-
-#define SIZE_T_MAX ((size_t)-1)
-#define SIZE_T_HALF_MAX ((SIZE_T_MAX >> 1) + 1)
-
 struct cfc_buffer_t
 {
         char *buf;
