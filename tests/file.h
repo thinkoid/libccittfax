5e7c0960af6f7cfde5ca3682e770cf737fc23ce1
Liviu Nicoara <lnicoara at thinkoid dot org>
2023-07-13 13:08:46 -0400

    fix: macro guards

--- tests/file.h
+++ tests/file.h
@@ -1,11 +1,11 @@
 /* -*- mode: c; -*- */
 
-#ifndef CCITTFAX_TESTS_CCITTFAX_H
-#define CCITTFAX_TESTS_CCITTFAX_H
+#ifndef CCITTFAX_TESTS_FILE_H
+#define CCITTFAX_TESTS_FILE_H
 
 #include <ccittfax/defs.h>
 
 unsigned char *
 load_file(const char *filename, size_t *srclen, struct cf_params_t *params);
 
-#endif /* CCITTFAX_TESTS_CCITTFAX_H */
+#endif /* CCITTFAX_TESTS_FILE_H */
