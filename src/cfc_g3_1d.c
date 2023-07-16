7bfd01c7e1499b3e0435368b69a1d2e77d4aab8a
Liviu Nicoara <lnicoara at thinkoid dot org>
2023-07-16 15:15:13 -0400

    chore: split 1D algorithm per line of input

--- src/cfc_g3_1d.c
+++ src/cfc_g3_1d.c
@@ -12,6 +12,12 @@
 #include "cfc_common.h"
 #include "cfc_tables.h"
 
+static inline void
+cf_byte_align(struct cf_buffer_t *buf)
+{
+        buf->pos = (buf->pos + 7) & ~7;
+}
+
 static inline int
 is_same_color(const char *arr, size_t pos, int color)
 {
@@ -26,53 +32,77 @@ get_rle(const char *arr, size_t pos, size_t end, int color)
         return cur - pos;
 }
 
-struct cf_buffer_t *
-cfc_g3_1d(const char *src, struct cf_params_t *params)
+static int
+cfc_g3_1d_line(struct cf_buffer_t *dst, struct cf_buffer_t *src,
+               struct cf_params_t *params)
 {
-        int line, a0, stride, rle, color;
-        struct cf_buffer_t *dst;
+        int color;
 
-        assert(params->columns);
+        size_t endpos = src->pos + params->columns;
+        assert(endpos <= (src->cap << 3));
+
+        for (color = 1; src->pos < endpos; color = !color) {
+                int rle = get_rle(src->buf, src->pos, endpos, color);
+
+                if (cfc_put_rle(dst, rle, color))
+                        return 1;
+
+                src->pos += rle;
+        }
+
+        return 0;
+}
+
+static struct cf_buffer_t *
+cfc_do_g3_1d(struct cf_buffer_t *dst, struct cf_buffer_t *src,
+             struct cf_params_t *params)
+{
+        int i;
+
+        cfc_put_eol(dst);
+
+        for (i = 0; i < params->rows; ++i) {
+                if (cfc_g3_1d_line(dst, src, params))
+                        goto err;
+
+                /* source row always starts at a byte boundary */
+                cf_byte_align(src);
+
+                if (params->end_of_line && cfc_put_eol(dst))
+                        goto err;
+
+                if (params->encoded_byte_align &&
+                    cfc_put_rle_explicit(dst, 0, (8 - (dst->pos & 7)) & 7))
+                        goto err;
+        }
+
+        if (params->end_of_block && cfc_put_eol_n(dst, 6))
+                goto err;
+
+        return dst;
+
+err:
+        free(dst->buf);
+        free(dst);
+
+        return 0;
+}
+
+struct cf_buffer_t *
+cfc_g3_1d(const char *buf, struct cf_params_t *params)
+{
+        struct cf_buffer_t src, *dst;
+
+        src.buf = (char *)buf;
+        src.cap = params->rows * ((params->columns + 7) & ~7);
+        src.pos = 0;
 
         dst = cf_make_buffer();
         if (0 == dst) {
                 fprintf(stderr, "malloc compression buffer : %s\n",
                         strerror(errno));
-                goto err;
+                return 0;
         }
 
-        cfc_put_eol(dst);
-
-        stride = (params->columns + 7) >> 3;
-        for (line = 0; line < params->rows; ++line, src += stride) {
-                color = 1;
-
-                for (a0 = 0; a0 < params->columns;) {
-                        rle = get_rle(src, a0, params->columns, color);
-                        if (cfc_put_rle(dst, rle, color))
-                                goto err;
-
-                        a0 += rle;
-                        color = !color;
-                }
-
-                if (params->end_of_line)
-                        cfc_put_eol(dst);
-
-                if (params->encoded_byte_align)
-                        cfc_put_rle_explicit(dst, 0, (8 - (dst->pos & 7)) & 7);
-        }
-
-        if (params->end_of_block)
-                cfc_put_eol_n(dst, 6);
-
-        return dst;
-
-err:
-        if (dst) {
-                free(dst->buf);
-                free(dst);
-        }
-
-        return 0;
+        return cfc_do_g3_1d(dst, &src, params);
 }
