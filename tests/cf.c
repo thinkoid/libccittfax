7eea80670833dc6802da01272b5b5cad106b6116
Liviu Nicoara <lnicoara@thinkoid.org>
2025-10-27 18:00:58 -0400

    feat: testing macros enhancements

--- tests/cf.c
+++ tests/cf.c
@@ -9,65 +9,111 @@
 
 #include "../src/cf.h"
 
+/**********************************************************************/
+
 struct test_t {
-        unsigned passed;
-        unsigned failed;
-        unsigned warned;
+        unsigned passed, failed, warned;
+        const char *name;
 };
 
+static struct test_t *g_test = 0;
+
 static inline void
-make_test(struct test_t *t)
+summarize_test(void)
 {
-        t->passed = t->failed = t->warned = 0;
+        const struct test_t *p = g_test;
+
+        if (0 == p)
+                return;
+
+        printf("\n# Summary for %s:\n", p->name);
+        printf("#   Passed : %u\n", p->passed);
+        printf("#   Failed : %u\n", p->failed);
+        printf("#   Warned : %u\n", p->warned);
+        printf("#   Total  : %u\n", p->passed + p->failed + p->warned);
+
+        fflush(stdout);
 }
 
-static inline void summarize_test(const struct test_t *t)
+static inline void
+make_test(struct test_t *p, const char *name)
 {
-        printf("# Summary:\n");
-        printf("#   Passed : %u\n", t->passed);
-        printf("#   Failed : %u\n", t->failed);
-        printf("#   Warned : %u\n", t->warned);
+        p->passed = p->failed = p->warned = 0;
+        p->name = name;
+        g_test = p;
+        atexit(summarize_test);
+        printf("# Starting test suite: %s\n", name);
 }
 
-#define TEST_DETAIL(t, expr, line, ...)                         \
-    do {                                                        \
-        if (!(expr)) {                                          \
-                printf("# Test failed, line: %d\n# ", line);    \
-                printf(__VA_ARGS__);                            \
-                printf("\n\n");                                 \
-                ++(t)->failed;                                  \
-        } else {                                                \
-                ++(t)->passed;                                  \
-        }                                                       \
-    } while(0)
+#define TEST_DETAIL(t, expr, line, fmt, ...)                            \
+        do {                                                            \
+                if (!(expr)) {                                          \
+                        printf("# FAIL [%s:%d]\n#   Expression: %s\n",  \
+                               (t)->name, line, #expr);                 \
+                        if (fmt && fmt[0]) {                            \
+                                printf("#   ");                         \
+                                printf(fmt, ##__VA_ARGS__);             \
+                                printf("\n");                           \
+                        }                                               \
+                        printf("\n");                                   \
+                        ++(t)->failed;                                  \
+                } else {                                                \
+                        ++(t)->passed;                                  \
+                }                                                       \
+        } while(0)
 
-#define WARN_DETAIL(t, expr, line, ...)                         \
-    do {                                                        \
-        if (!(expr)) {                                          \
-                printf("# Warning, line: %d\n# ", line);        \
-                printf(__VA_ARGS__);                            \
-                printf("\n\n");                                 \
-                ++(t)->warned;                                  \
-        }                                                       \
-    } while(0)
+#define WARN_DETAIL(t, expr, line, fmt, ...)                           \
+        do {                                                           \
+                if (!(expr)) {                                         \
+                        printf("# WARN [%s:%d]\n#   Expression: %s\n", \
+                               (t)->name, line, #expr);                \
+                        if (fmt && fmt[0]) {                           \
+                                printf("#   ");                        \
+                                printf(fmt, ##__VA_ARGS__);            \
+                                printf("\n");                          \
+                        }                                              \
+                        printf("\n");                                  \
+                        ++(t)->warned;                                 \
+                }                                                      \
+        } while (0)
 
-#define TEST(t, expr, ...) TEST_DETAIL(t, expr, __LINE__, __VA_ARGS__)
-#define WARN(t, expr, ...) WARN_DETAIL(t, expr, __LINE__, __VA_ARGS__)
+/* These allow the message to be optional */
+#define TEST(t, expr, ...) TEST_DETAIL(t, expr, __LINE__, "" __VA_ARGS__)
+#define WARN(t, expr, ...) WARN_DETAIL(t, expr, __LINE__, "" __VA_ARGS__)
+
+#define NOTE(x)                       \
+        do {                          \
+                printf("\n# Note: "); \
+                printf(x);            \
+                printf("\n\n");       \
+        } while (0)
+
+#define STEP(x)                       \
+        do {                          \
+                printf("\n# Step: "); \
+                printf x;             \
+                printf("\n\n");       \
+        } while (0)
+
+/**********************************************************************/
 
 static void
 test_make_buffer(struct test_t *test)
 {
-        struct cf_buffer_t *ptr = cf_make_buffer();
+        struct cf_buffer_t *cf_buffer = cf_make_buffer();
 
-        TEST(test, ptr, "expected cf_make_buffer success");
-        TEST(test, ptr->buf, "expected valid cf_buffer_t");
-        TEST(test, ptr->cap > 0, "expected non-zero cf_buffer_t capacity");
-        TEST(test, ptr->pos == 0, "expected cf_buffer_t position 0");
+        NOTE("testing cf_make_buffer");
 
-        if(ptr) {
-                if(ptr->buf)
-                        free(ptr->buf);
-                free(ptr);
+        TEST(test, 0 != cf_buffer);
+        TEST(test, 0 != cf_buffer->buf);
+
+        TEST(test, cf_buffer->cap > 0);
+        TEST(test, cf_buffer->pos == 0);
+
+        if(cf_buffer) {
+                if(cf_buffer->buf)
+                        free(cf_buffer->buf);
+                free(cf_buffer);
         }
 }
 
@@ -99,50 +145,43 @@ static void
 do_test_resize_buffer(struct test_t *test, size_t cap, size_t pos,
                       int resized)
 {
-        struct cf_buffer_t cf_buf = { 0 }, *pcf_buf;
+        struct cf_buffer_t cf_buffer = { 0 }, *resized_cf_buffer;
 
-        char *p = malloc(cap);
-        if (0 == p)
+        char *custom_buffer = malloc(cap);
+        if (0 == custom_buffer)
                 goto fail;
 
-        memset(p, 0, cap);
+        memset(custom_buffer, 0, cap);
 
-        cf_buf.buf = p;
+        cf_buffer.buf = custom_buffer;
 
-        cf_buf.cap = cap;
-        cf_buf.pos = pos;
+        cf_buffer.cap = cap;
+        cf_buffer.pos = pos;
 
-        cf_setbits(cf_buf.buf, 0, pos, 1);
-        /* dump_buffer(cf_buf.buf, cap); */
+        cf_setbits(cf_buffer.buf, 0, pos, 1);
+        /* dump_buffer(cf_buffer.buf, cap); */
 
-        pcf_buf = cf_resize_buffer(&cf_buf);
+        resized_cf_buffer = cf_resize_buffer(&cf_buffer);
 
-        TEST(test, pcf_buf && pcf_buf == &cf_buf,
-             "expected cf_resize_buffer success");
-
-        TEST(test, resized ^ (pcf_buf->buf == p),
-             "expected %s cf_buffer_t::buf", resized ? "new" : "same");
-
-        TEST(test, resized ^ (pcf_buf->cap == cap),
-             "expected %s cf_buffer_t::cap", resized ? "increased" : "same");
-
-        TEST(test, pos == pcf_buf->pos,
-             "expected cf_buffer_t::pos %lu; got %lu", pos, pcf_buf->pos);
+        TEST(test, resized_cf_buffer && resized_cf_buffer == &cf_buffer);
+        TEST(test, resized ^ (resized_cf_buffer->buf == custom_buffer));
+        TEST(test, resized ^ (resized_cf_buffer->cap == cap));
+        TEST(test, pos == resized_cf_buffer->pos);
 
         if(resized) {
-                test_buffer_color(test, pcf_buf->buf, 0, pos, 1);
-                test_buffer_color(test, pcf_buf->buf, pos, cap << 3, 0);
+                test_buffer_color(test, resized_cf_buffer->buf, 0, pos, 1);
+                test_buffer_color(test, resized_cf_buffer->buf, pos, cap << 3, 0);
         }
 
-        free(pcf_buf->buf);
+        free(resized_cf_buffer->buf);
 
-        if (pcf_buf != &cf_buf)
-                free(pcf_buf);
+        if (resized_cf_buffer != &cf_buffer)
+                free(resized_cf_buffer);
 
         return;
 
 fail:
-        TEST(test, 0, "do_test_simple_resize_buffer fail");
+        TEST(test, 0 != custom_buffer);
 }
 
 static void
@@ -168,15 +207,10 @@ test_buffer(struct test_t *test)
 
 int main()
 {
-        struct test_t test;
-        make_test(&test);
+        static struct test_t test;
+        make_test(&test, "internal helper API test");
 
         test_buffer(&test);
 
-        printf("# Summary:\n");
-        printf("# --> tests:  %u\n", test.passed);
-        printf("# --> failed: %u\n", test.failed);
-        printf("# --> warned: %u\n", test.warned);
-
-        return 0;
+        return !!test.failed;
 }
