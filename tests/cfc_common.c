35e34dbe7ac67a17c8feb8a8eb1201c024ff25aa
Liviu Nicoara <lnicoara@thinkoid.org>
2025-10-30 14:37:19 -0400

    test: shift RLE encoding starting position

--- tests/cfc_common.c
+++ tests/cfc_common.c
@@ -15,7 +15,7 @@
 static inline void
 test_cfc_put_rle(struct test_t *t)
 {
-        size_t rle, rle_ver;
+        size_t pos, rle, rle_ver;
         struct cf_buffer_t cf_buffer = { 0, 0, 0 };
 
         char buf[256];
@@ -23,19 +23,22 @@ test_cfc_put_rle(struct test_t *t)
 
         cf_buffer.buf = buf;
         cf_buffer.cap = sizeof buf;
-        cf_buffer.pos = 0;
 
-        for(rle = 0; rle < 16384; ++rle) {
-                cfc_put_rle(&cf_buffer, rle, 0);
-                /* dump_buffer(cf_buffer.buf, cf_buffer.cap); */
+        for(pos = 0; pos < sizeof(unsigned) << 3; pos++) {
+                /* shift starting position at each iteration */
+                for(rle = 0; rle < 16384; ++rle) {
+                        cf_buffer.pos = pos;
+                        cfc_put_rle(&cf_buffer, rle, 0);
+                        /* dump_buffer(cf_buffer.buf, cf_buffer.cap); */
 
-                cf_buffer.pos = 0;
-                rle_ver = get_rle(&cf_buffer, 0);
+                        cf_buffer.pos = pos;
+                        rle_ver = get_rle(&cf_buffer, 0);
 
-                TEST(t, rle == rle_ver, "expected rle %lu, got %lu", rle, rle_ver);
+                        TEST(t, rle == rle_ver, "expected rle %lu, got %lu",
+                             rle, rle_ver);
 
-                cf_buffer.pos = 0;
-                memset(buf, 0, sizeof buf);
+                        memset(buf, 0, sizeof buf);
+                }
         }
 }
 
